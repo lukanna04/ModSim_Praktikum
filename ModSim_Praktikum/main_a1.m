b2419c4781e2e569ae25625a5b1660f6db785264
lukanna04 <jo.kruger@icloud.com>
2026-05-12 13:18:21 +0200

    Dateien hinzugefuegt

---
+++ ModSim_Praktikum/main_a1.m
@@ -0,0 +1,94 @@
+% MODSIM Laborpraktikum, 1. Aufgabe
+% Prof. K. Janschek, Dr.-Ing. Th. Range, Dr.-Ing. E. Dueblenk
+%
+% edit: Johanna Krüger, Arne Noack, Viktor Strichow
+%
+% main_a1.m - Realisierung der VPG-Methode mit Fehlerschätzung
+% für PT1-Glied
+% zu ergänzende Codezeilen sind mit ">>> ergänzen ...." und ..."gekennzeichnet
+
+clear all % Lösche Arbeitsspeicher
+
+Tm = 10; % Konstante des PT1, s
+
+h = 0.1; % Schrittweite, s
+
+t0 = 0; % Integrationsbeginn, s
+tf = 300; % Integrationsende, s
+
+t = []; % Zeitwerte für Plot, s
+d = []; % Fehler-Schätzwerte
+u = []; % Stellwerte u(t)
+y = []; % Ausgangswerte y(t)
+ys = []; % Soll-Ausgangswerte y_soll(t)
+
+% Initialisierung
+[dum,x(1)] = system_pt1([],[],[],0);
+d(1) = 0;
+
+% Integration nach VPG-Methode
+ti = t0;
+i = 1;
+
+while ti <= tf
+    % Berechnung des Soll-Ausgangswertes
+    if ti < 1
+        ys(i) = 0;
+    elseif ti >= 1
+        ys(i) = 5 * (1 - exp(-1/Tm * ti));
+    end
+
+    % Berechnung des Stellwertes
+    if ti < 1
+        u(i) = 0;
+    elseif ti >= 1
+        u(i) = 5;
+    end
+
+    if ti + h/2 < 1
+        u_h2 = 0;
+    else
+        u_h2 = 5;
+    end
+
+    if ti + h < 1
+        u_h = 0;
+    else
+        u_h = 5;
+    end
+ 
+
+    % Berechnung des Ausgangswertes
+    y(i) = system_pt1( ti , x(i) , u(i) , 3); %die Parameter einsetzen
+
+    % Berechnung der Koeffizienten für VPG-Methode
+    k1 = system_pt1( ti , x(i) , u(i) , 1); %die Parameter einsetzen
+    k2 = system_pt1( ti + h/2, x(i) + h/2 * k1, u_h2 , 1); %die Parameter einsetzen
+    k3 = system_pt1( ti + h , x(i) - h*k1 + 2*h*k2 , u_h , 1); %die Parameter einsetzen
+
+    % Wichtiger Hinweis: Die Parameter bei den Aufrufen von system_pt1(...) müssen unter Beachtung von jeweiligen Zeitpunkten bestimmt werden!
+    
+    % Berechnung des Zustands-Schätzwertes x(ti+h)
+    x(i+1) = x(i) + h/6 * (k1 + 4*k2 + k3);
+
+    % Berechnung der LDF Fehlerabschätzung d(ti+h)
+    d(i+1) = x(i + 1) - x(i) - h * y(i);
+    t(i) = ti; % Zeitwert für Plot speichern
+    ti = ti + h; % Zeitvariable um einen Schritt erhöhen
+    i = i + 1; % Index inkrementieren
+end
+
+d = d(1:end-1);
+result = [t;d];
+
+% Anzeige der Ergebnisse
+figure(1);
+subplot(2,1,1); plot(t,u); title('Eingang PT1-Glied');zoom on;grid on;
+subplot(2,1,2); plot(t,y); title('Ausgang PT1-Glied');zoom on;grid on;
+xlabel('Zeit, s');
+
+figure(2);
+subplot(2,1,1); plot(t,y-ys,'.-'); title('GDF berechnet');zoom on;grid on;
+tit=sprintf('LDF geschätzt: max. Betrag = %g',max(abs(d)));
+subplot(2,1,2); plot(t,d,'.-'); title(tit);zoom on;grid on;
+xlabel('Zeit, s');
---
+++ ModSim_Praktikum/system_pt1.m
@@ -0,0 +1,44 @@
+% MODSIM Laborpraktikum, 1. Aufgabe
+%
+% Dr.-Ing. Th. Range, Dr.-Ing. E. Dueblenk
+%
+% edit: Johanna Krüger, Arne Noack, Viktor Strichow
+%
+% zu ergänzende Codezeilen sind mit ">>> ergänzen ...." gekennzeichnet
+% Berechnung des Systems "PT1-Glied"
+%
+% (Hinweis: Die Struktur des Programms erlaubt eine Einbindung in
+% SIMULINK als sog. S-Function; dies ist jedoch im Rahmen des
+% Praktikums nicht vorgesehen!)
+
+function [sys, x0] = system_pt1(t, x, u, flag )
+
+% Eingabe-Parameter:
+% t - Zeit
+% x - x(t) Zustandsvektor zum Zeitpunkt t
+% u - u(t) Vektor der Eingangssignale zum Zeitpnukt t
+% flag - Steuerparameter, legt fest, welche Ausgabe
+% gefordert wird:
+
+Tm = 10; % Zeitkonstante des PT1-Gliedes
+
+if flag == 0 % Ausgabe der Anfangswerte für den Zustand auf Vektor x0
+    x0 = 0;
+    sys = [1,0,1,1,0,0]; % diese Zeile ist nur für Simulink nötig, sie gilt so NUR in diesem Beispiel!
+
+elseif abs(flag) == 1 % Ausgabe der Ableitungen von x auf Vektor sys = x' = f(x(t),u(t),t)
+    % G(s) = 1 / (1 + Tm*s)
+    % Y*Tm*s +Y = U 
+    % Zustandsdifferentialgleichung:
+    % y' = - 1/Tm * y + 1/Tm * u
+    % y -> x
+    sys = (u - x) * 1/Tm;
+
+elseif flag == 3 % Ausgabe der Ausgangswerte des Systems auf Vektor sys: sys = y = g(x(t),u(t),t)
+    % sys = x;
+    sys = u * ( 1 - exp(-1/Tm * t));
+
+else % bei anderen Flagwerten nichts ausgeben
+    sys = [];
+
+end
