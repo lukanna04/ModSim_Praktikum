7bcb89503e7cfb69383ee13adb0894ff2ae24193
Louise <louise.perrin@mailbox.tu-dresden.de>
2026-06-08 16:25:04 +0000

    Signalflussplan korrigiert und Simulationsfehler behoben

--- ModSim_Praktikum/Praktikumsaufgabe_2/variablen.m
+++ ModSim_Praktikum/Praktikumsaufgabe_2/variablen.m
@@ -1,22 +1,38 @@
+% Leistungsstufe
 K_L = 1;
 i_vmax = 1;
+
+% Servoventil
 K_sv = 0.796;
-F_N =  63000;
-b1 = 2.39 * 10^6;
-c_o = 36.5 * 10^6;
+Ksv  = K_sv;      % Alias
+
+F_N = 63000;
+FN  = F_N;        % Alias
+
+% Prüfzylinder
+b1  = 2.39e6;
+
+c_o  = 36.5e6;
+c_oe = c_o;       % Alias
+
+% Massen
 m_k = 8.7;
+mk  = m_k;        % Alias
+
 m_p = 260;
-c_p = 75 + 10^6;
+mp  = m_p;        % Alias
 
 m_g = m_k + m_p;
+mg  = m_g;        % Alias
 
-K_F = K_L * K_sv * b1 ;
+% Prüfling
+c_p = 75e6;
+cp  = c_p;        % Alias
+
+% Parameter der linearen Übertragungsfunktion
+K_F = K_L * K_sv * b1 / c_p;
 
 a1 = b1/c_p + b1/c_o;
-
 a2 = m_g/c_p;
-
-a3 = m_g * b1 / (c_p * c_o);
-
-
+a3 = m_g*b1/(c_p*c_o);
 
